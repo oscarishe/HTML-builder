99586da02e16e3f7a2870a266e8279459e6f52d0
oscarishe <oscar.palmero228@gmail.com>
2022-05-16 20:15:53 +0300

    feat: complete 4rd task (Mon, May 16, 2022 08:15:52 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,52 @@
+const fs = require('fs');
+const { resolve } = require('path');
+const path = require('path');
+let copyFolder = __dirname+'\\files\\';
+let newFolder = __dirname+'\\files-copy\\';
 
+const createDirectory = () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'),
+    { recursive: true }, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+    });
+};
+const clearDirectory = () => {
+  fs.readdir(newFolder, (err, files) => {
+    if (err) throw err;
+  
+    for (const file of files) {
+      fs.unlink(path.join(newFolder, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
+};
+const rewriteDirectory = () => {
+  fs.readdir(copyFolder, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        fs.copyFile(copyFolder + file, newFolder + file, (err) => {
+          if (err) throw err;
+          console.log(`${file} успешно скопирован`);
+        });
+      });
+    }
+  });
+};
+const promise = new Promise((resolve) => {
+  createDirectory();
+  resolve();
+}
+);
+promise.then( () => {
+  clearDirectory();
+  resolve();
+}
+).then (()=> {
+  rewriteDirectory();
+  resolve();
+});
